97d2e2da1de00ce306858aa32c29b9446f067d29
Ruka Okuyama <rukaokuyama@gmail.com>
2023-06-28 14:31:45 +0900

    added test for search bar

--- cypress/e2e/ghibilitest.cy.js
+++ cypress/e2e/ghibilitest.cy.js
@@ -1,6 +1,19 @@
-describe('Studio Ghibili Filmography', () => {
-  it('Displays film details when a film thumbnail is clicked',() => {
+describe('Studio Ghibli Filmography', () => {
+  it('Displays film details when a film thumbnail is clicked', () => {
     cy.visit('http://localhost:5500');
 
+    // test for thumbnail
+    cy.get('.film-thumbnail').first().click();
+
+    // verify filmtitle and film detail
+    cy.get('#filmTitle').should('exist');
+    cy.get('#filmDetails').should('exist');
+  });
+
+  it('Filters films based on search input',() => {
+    cy.visit('http://localhost:5500');
+
+    // search for totoro in the search bar
+    cy.get('#searchBar').type('My Neighbor Totoro');
   })
 });
