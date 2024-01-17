ceaee45307fb951ef3e06e627ba9695cebf00ec0
Ruka Okuyama <rukaokuyama@gmail.com>
2024-01-18 00:41:27 +0900

    added package.js

--- package.js
+++ package.js
@@ -1,11 +1,5 @@
 {
-  "name": "studio-ghibli",
-  "version": "1.0.0",
-  "engines": {
-    "node": "16.15.1"
-  },
-  "scripts": {
-    "start": "app.js"
-  },
-  // other package.json configurations...
-}
+  "engines"; {
+    "node"; "16.15.1"
+  }
+};
